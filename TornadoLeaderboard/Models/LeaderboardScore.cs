9b68f02abb011c8239dc816d64a5981db05d41d1
Chip Lambert <chiplambert@crusader-games.com>
2018-09-06 19:13:09 -0400

    Initial code

---
+++ TornadoLeaderboard/Models/LeaderboardScore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TornadoLeaderboard.Models
+{
+    public class LeaderboardScore
+    {
+        public string Username { get; set; }
+        public int Score { get; set; }
+        public DateTime ScoreDate { get; set; }
+        public bool IAPFlag { get; set; }
+    }
+}
---
+++ TornadoLeaderboard/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using TornadoLeaderboard.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TornadoLeaderboard
+{
+    public class Startup
+    {
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<LeaderboardContext>(opt => opt.UseInMemoryDatabase("LeaderboardDB"));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
