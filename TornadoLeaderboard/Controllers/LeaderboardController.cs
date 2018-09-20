3a376c4770cadccb9e30e866196cf2908f5693d8
Chip Lambert <chiplambert@crusader-games.com>
2018-09-20 18:57:49 -0400

    Working API

---
+++ TornadoLeaderboard/Controllers/LeaderboardController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TornadoLeaderboard.Models;
+
+namespace TornadoLeaderboard.Controllers
+{
+    [Route("api/leaderboard")]
+    [ApiController]
+    public class LeaderboardController : ControllerBase
+    {
+        private readonly LeaderboardContext _context;
+
+        public LeaderboardController(LeaderboardContext context)
+        {
+            _context = context;
+
+            if (_context.LeaderboardScores.Count() == 0)
+            {
+                //Create new score if empty
+                _context.LeaderboardScores.Add(new LeaderboardScore { UserGOID = 28, Username = "chiplambert", IAPFlag = true, Score = 25000 });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<List<LeaderboardScore>> GetAll()
+        {
+            return _context.LeaderboardScores.ToList();
+        }
+
+        [HttpGet("{id}", Name ="GetScores")]
+        public ActionResult<LeaderboardScore> GetById(long id)
+        {
+            var score = _context.LeaderboardScores.Find(id);
+            if (score == null)
+            {
+                return NotFound();
+            }
+            return score;
+        }
+
+        [HttpPost]
+        public IActionResult Create(LeaderboardScore score)
+        {
+            _context.LeaderboardScores.Add(score);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetScores", new { id = score.UserGOID }, score);
+        }
+
+
+    }
+}
--- TornadoLeaderboard/Models/LeaderboardScore.cs
+++ TornadoLeaderboard/Models/LeaderboardScore.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,11 @@ namespace TornadoLeaderboard.Models
 {
     public class LeaderboardScore
     {
+        [Key]
+        public long UserGOID { get; set; }
         public string Username { get; set; }
         public int Score { get; set; }
-        public DateTime ScoreDate { get; set; }
+        //public DateTime ScoreDate { get; set; }
         public bool IAPFlag { get; set; }
     }
 }
